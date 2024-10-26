593257b3c715048260c7c15052936571984f3cb6
Anagh Kansra <kansraanagh1r@gmail.com>
2024-10-26 08:42:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,14 +12,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//static files access
-app.use(express.static(path.join(__dirname, './client/build')));
+//static access files
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
 
-app.get('*', function(req,res){
-  res.sendFile(path.join(__dirname, './client/build/index.html'))
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 //port
 const PORT = process.env.PORT || 8080;
